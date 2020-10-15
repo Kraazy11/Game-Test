0df2bce6a4a1362169e365d7e06cf54f032f808b
Kraazy11 <zachl99@hotmail.com>
2020-10-15 18:53:00 -0400

    Atelier 5

---
+++ Assets/Script/Test1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    private float lifeRemaining;
+    public int maxLife = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeRemaining = maxLife;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeRemaining = lifeRemaining - 1;
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
