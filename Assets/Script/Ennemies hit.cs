d10b0672570b443b59a37bf71103d867586d3eaf
Kraazy11 <zachl99@hotmail.com>
2020-10-29 12:59:00 -0400

    atelier 30 oct

---
+++ Assets/Script/Ennemies hit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemieshit : MonoBehaviour
+{
+    public float dmg = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.GetComponent<LifeDemo>().HurtAndReset(dmg);
+        }
+}
